9abe4df1184a3a7cdcf1a4947243dbf5774d1290
ztor2 <ztor76@gmail.com>
2022-01-04 23:04:21 +0900

    update

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def textgen_option(temperature, avg_length):
     config = json.load(open('./model/config.json', 'r'))
     config['task_specific_params']['text-generation']['temperature'] = temperature
     config['task_specific_params']['text-generation']['max_length'] = \
-        random.sample(range(avg_length - 20, avg_length + 21), 1)[0]
+        random.sample(range(avg_length - 10, avg_length + 11), 1)[0]
     json.dump(config, open('./model/config.json', 'w'))
     return print('temperature: {}, avg_length: {}'.format(temperature, avg_length))
 
